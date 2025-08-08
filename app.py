f9cec9d40802baba6fdb158f6d320b5c72cea93b
profpreyan <77844196+profpreyan@users.noreply.github.com>
2025-08-08 16:40:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -218,4 +218,5 @@ def redirect_slug(slug):
     return redirect(link.target_url, code=302)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
